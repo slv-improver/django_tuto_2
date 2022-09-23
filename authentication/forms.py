c8bd7cd0a0d1d1399247515fcdde9761333bb31d
slv-improver <k.moret.slv@gmail.com>
2022-09-23 22:50:55 +0100

    fix bug: wrong use of forms module

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='Nom d\'utilisateur')
-    password = form.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
