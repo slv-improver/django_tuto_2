83b2e04d14594cbf54958ad851d5ccab7684a07b
slv-improver <k.moret.slv@gmail.com>
2022-09-23 21:30:19 +0100

    create LoginForm

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # FOTOBLOG project
 
 OpenClassrooms course
+
+## App configuration
+
+To create a personnalized User model, extend the User class from AbstractUser (or AbstractBaseUser to get an empty User model)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+def login_page(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = forms.LoginForm()
+    return render(
+        request,
+        'authentication/login.html',
+        {'form': form}
+    )
