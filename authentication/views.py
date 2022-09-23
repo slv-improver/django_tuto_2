86f4d26adaaac79db6c7aebb77c289cd83e45433
slv-improver <k.moret.slv@gmail.com>
2022-09-23 22:09:11 +0100

    login user after auth

--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,25 @@
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render
 from . import forms
 
 def login_page(request):
+    message = ''
     if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            pass
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+            else:
+                message = 'Identifiants invalides.'
     else:
         form = forms.LoginForm()
     return render(
         request,
         'authentication/login.html',
-        {'form': form}
+        {'form': form, 'message': message}
     )
