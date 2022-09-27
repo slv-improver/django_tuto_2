7cf0018924efa9c6f99b010959a0ea6df5d7f215
slv-improver <k.moret.slv@gmail.com>
2022-09-27 23:09:22 +0100

    switch login_page to class-based views

--- authentication/views.py
+++ authentication/views.py
@@ -1,29 +1,39 @@
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
+from django.views.generic import View
 from . import forms
 
-def logout_user(request):
-    logout(request)
+def logout_user(r):
+    logout(r)
     return redirect('login')
 
-def login_page(request):
-    message = ''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
+
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, r):
+        form = self.form_class()
+        message = ''
+        return render(
+            r,
+            self.template_name,
+            {'form': form, 'message': message}
+        )
+    
+    def post(self, r):
+        form = self.form_class(r.POST)
         if form.is_valid():
             user = authenticate(
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password'],
             )
             if user is not None:
-                login(request, user)
+                login(r, user)
                 return redirect('home')
-            else:
-                message = 'Invalid credentials .'
-    else:
-        form = forms.LoginForm()
-    return render(
-        request,
-        'authentication/login.html',
-        {'form': form, 'message': message}
-    )
+        message = 'Invalid credentials .'
+        return render(
+            r,
+            'authentication/login.html',
+            {'form': form, 'message': message}
+        )
