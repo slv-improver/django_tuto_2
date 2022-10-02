c5e259601ed52fec3223efdbc9db89a9c9d3631e
slv-improver <k.moret.slv@gmail.com>
2022-10-02 13:43:31 +0100

    add own validator

---
+++ authentication/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+
+class ContainsLetterValidator:
+    def validate(self, password, user=None):
+        if not any(char.isalpha() for char in password):
+            raise ValidationError(
+                'The password must contain a letter',
+                code='password_no_letter'
+            )
+
+    def get_help_text(self):
+        return 'Your password must contain at least one letter upper or lowercase.'
--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -101,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     # {
     #     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     # },
+    {
+        'NAME': 'authentication.validators.ContainsLetterValidator',
+    },
 ]
 
 
