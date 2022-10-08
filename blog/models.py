d28b212ada6972ecb46217053245210f8f5e79ab
slv-improver <k.moret.slv@gmail.com>
2022-10-08 16:24:15 +0100

    remove author form Blog

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,6 @@ class Blog(models.Model):
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
     title = models.CharField(max_length=128)
     content = models.CharField(max_length=5000)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     contributors = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         through='BlogContributor',
