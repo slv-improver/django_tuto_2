7d1c932a999b07bfad67c864be7bfb9fdb954d4f
slv-improver <k.moret.slv@gmail.com>
2022-10-11 00:24:31 +0100

    add customize filter for post creation delta

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils import timezone
 
 
 register = template.Library()
@@ -12,3 +13,13 @@ def get_poster_display(context, user):
     if user == context['user']:
         return 'me'
     return user.username
+
+@register.filter
+def get_posted_at_display(time):
+    posted_before = timezone.now() - time # .total_seconds() to work with delta in sec
+    if posted_before.days > 0:
+        return f'at {time.strftime("%H:%M %d-%b-%y")}'
+    elif posted_before.seconds > 3599:
+        return f'before {posted_before.seconds // 3600} h'
+    else:
+        return f'before {posted_before.seconds // 60} min'
